7235112652ec36084f1c8ea0032379bbc218679c
Rival Ramadhan <rivalramadhan97@gmail.com>
2024-12-07 13:15:38 +0700

    feat: update const

--- lib/config.dart
+++ lib/config.dart
@@ -1,14 +1,8 @@
 class AppConfig {
-// //IP PROD
-  // static const String ip = "172.27.77.125";
-  // static const String port = "9000";
-
-  //URL API
-  // static const String baseUrl = "http://$ip:$port/api/";
-
-  //URL DOMAIN DEV
-  // static const String domain = "https://ksu-backend.vercel.app";
+  //local
   // static const String domain = "http://localhost:3000";
-  static const String domain = "http://192.168.137.2:3000";
-  // static const String domain = "http://34.101.229.231:3000";
+  //prod
+  // static const String domain = "http://192.168.1.200:3000";
+  //public
+  static const String domain = "http://34.101.229.231:3000";
 }
