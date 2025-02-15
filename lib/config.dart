8f126244da6a9d47a06dc6b8d61c659eaee7f91f
Rival Ramadhan <rivalramadhan97@gmail.com>
2025-02-15 14:09:10 +0700

    feat: perbaikan produk

--- lib/config.dart
+++ lib/config.dart
@@ -2,9 +2,9 @@ class AppConfig {
   //local
   // static const String domain = "http://10.44.121.101:3000";
   //local
-  // static const String domain = "http://localhost:3000";
+  static const String domain = "http://localhost:3000";
   //prod
-  static const String domain = "http://192.168.1.200:3000";
+  // static const String domain = "http://192.168.1.200:3000";
   //public
   // static const String domain = "http://34.101.229.231:3000";
 }
